ec9aa1c297f693d48145f12028bc85b81fa5554b
kyubuns <kyubuns@gmail.com>
2020-06-27 23:54:35 +0900

    refactor

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -34,12 +34,11 @@ namespace ShapeTest
                         TranslateTo.LocalPositionX(discObject)
                     ),
                     Anime.Play(
-                        new Vector3EasingAnimatorMid(
-                            new InOutCirc(),
+                        MyEasing.Create<InOutCirc>(
                             new Vector3(0f, 0f, 0f),
                             new Vector3(2.5f, 2.5f, 2.5f),
                             new Vector3(0f, 0f, 0f),
-                        duration),
+                            duration),
                         TranslateTo.LocalScale(discObject)
                     )
                 );
@@ -60,41 +59,49 @@ namespace ShapeTest
         }
     }
 
-public class Vector3EasingAnimatorMid : IAnimator<Vector3>
-{
-    private readonly IEasing easing;
-    private readonly Vector3 start;
-    private readonly Vector3 mid;
-    private readonly Vector3 end;
-    private readonly float duration;
-
-    public Vector3EasingAnimatorMid(IEasing easing, Vector3 start, Vector3 mid, Vector3 end, float duration)
+    public static class MyEasing
     {
-        this.easing = easing;
-        this.start = start;
-        this.mid = mid;
-        this.end = end;
-        this.duration = duration;
-    }
-
-    public void Start()
-    {
-    }
-
-    public Tuple<Vector3, bool> Update(float time)
-    {
-        var t = Mathf.Min(time / duration, 1f);
-        if (t < 0.5f)
+        public static Vector3EasingAnimatorMid Create<T>(Vector3 start, Vector3 mid, Vector3 end, float duration) where T : IEasing, new()
         {
-            t = t * 2f;
-            return Tuple.Create(Vector3.LerpUnclamped(start, mid, easing.Function(t)), time > (double) duration);
-        }
-        else
-        {
-            t = (t - 0.5f) * 2f;
-            return Tuple.Create(Vector3.LerpUnclamped(mid, end, easing.Function(t)), time > (double) duration);
+            return new Vector3EasingAnimatorMid(new T(), start, mid, end, duration);
         }
     }
-}
+
+    public class Vector3EasingAnimatorMid : IAnimator<Vector3>
+    {
+        private readonly IEasing easing;
+        private readonly Vector3 start;
+        private readonly Vector3 mid;
+        private readonly Vector3 end;
+        private readonly float duration;
+
+        public Vector3EasingAnimatorMid(IEasing easing, Vector3 start, Vector3 mid, Vector3 end, float duration)
+        {
+            this.easing = easing;
+            this.start = start;
+            this.mid = mid;
+            this.end = end;
+            this.duration = duration;
+        }
+
+        public void Start()
+        {
+        }
+
+        public Tuple<Vector3, bool> Update(float time)
+        {
+            var t = Mathf.Min(time / duration, 1f);
+            if (t < 0.5f)
+            {
+                t = t * 2f;
+                return Tuple.Create(Vector3.LerpUnclamped(start, mid, easing.Function(t)), time > (double) duration);
+            }
+            else
+            {
+                t = (t - 0.5f) * 2f;
+                return Tuple.Create(Vector3.LerpUnclamped(mid, end, easing.Function(t)), time > (double) duration);
+            }
+        }
+    }
 }
 
