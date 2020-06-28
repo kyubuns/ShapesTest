c59e6ab8356c90cc709a21196634f662ab21049c
kyubuns <kyubuns@gmail.com>
2020-06-28 14:26:18 +0900

    撮影用

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -3,7 +3,7 @@ using Cysharp.Threading.Tasks;
 using Shapes;
 using UnityEditor;
 using UnityEngine;
-using System.Linq;
+using Animator = AnimeTask.Animator;
 
 namespace ShapeTest
 {
@@ -22,15 +22,100 @@ namespace ShapeTest
             {
                 Debug.Log("Main Start");
 
-                await Anime.Delay(0.1f);
-                await Animation();
+                await Sample2();
 
                 if (playOnce) break;
             }
             EditorApplication.isPlaying = false;
         }
 
-        private async UniTask Animation()
+        private static async UniTask Sample1()
+        {
+            using (var shapeObject = new DisposableGameObject(new Vector3(-5f, 0f, 0f)))
+            {
+                var shape = shapeObject.AddComponent<Rectangle>();
+                shape.Type = Rectangle.RectangleType.RoundedHollow;
+                shape.Color = new Color32(255, 242, 173, 255);
+                shape.Thickness = 0.1f;
+
+                var cube = shape;
+
+                await Anime.Delay(0.3f);
+                await Anime.Play(
+                    Easing.Create<Linear>(new Vector3(-5f, 0f, 0f), new Vector3(5f, 0f, 0f), 2f),
+                    TranslateTo.LocalPosition(cube)
+                );
+                await Anime.Delay(0.3f);
+            }
+        }
+
+        private static async UniTask Sample2()
+        {
+            using (var shapeObject = new DisposableGameObject(new Vector3(1f, -2f, 0f)))
+            {
+                var shape = shapeObject.AddComponent<Rectangle>();
+                shape.Type = Rectangle.RectangleType.RoundedHollow;
+                shape.Color = new Color32(255, 242, 173, 255);
+                shape.Thickness = 0.1f;
+
+                var cube = shape;
+
+                await Anime.Delay(0.3f);
+                await Anime.PlayTo(
+                    Easing.Create<Linear>(new Vector3(-5f, 3f, 0f), 2f),
+                    TranslateTo.LocalPosition(cube)
+                );
+                await Anime.Delay(0.3f);
+            }
+        }
+
+        private static async UniTask Sample3()
+        {
+            using (var shapeObject = new DisposableGameObject(new Vector3(1f, -2f, 0f)))
+            {
+                var shape = shapeObject.AddComponent<Rectangle>();
+                shape.Type = Rectangle.RectangleType.RoundedHollow;
+                shape.Color = new Color32(255, 242, 173, 255);
+                shape.Thickness = 0.1f;
+
+                var cube = shape;
+
+                await Anime.Delay(0.3f);
+                await Anime.PlayTo(
+                    Easing.Create<InCubic>(new Vector3(-5f, 3f, 0f), 2f),
+                    TranslateTo.LocalPosition(cube)
+                );
+                await Anime.Delay(0.3f);
+            }
+        }
+
+        private static async UniTask Sample4()
+        {
+            using (var shapeObject = new DisposableGameObject(new Vector3(-3f, 0f, 0f)))
+            {
+                var shape = shapeObject.AddComponent<Rectangle>();
+                shape.Type = Rectangle.RectangleType.RoundedHollow;
+                shape.Color = new Color32(255, 242, 173, 255);
+                shape.Thickness = 0.1f;
+
+                var cube = shape;
+
+                await Anime.Delay(0.3f);
+                await UniTask.WhenAll(
+                    Anime.PlayTo(
+                        Moving.Linear(3f, 2f),
+                        TranslateTo.LocalPositionX(cube)
+                    ),
+                    Anime.PlayTo(
+                        Animator.Delay(1.8f, Easing.Create<Linear>(Vector3.zero, 0.2f)),
+                        TranslateTo.LocalScale(cube)
+                    )
+                );
+                await Anime.Delay(0.3f);
+            }
+        }
+
+        private static async UniTask GravityTest()
         {
             while (true)
             {
@@ -39,7 +124,7 @@ namespace ShapeTest
             }
         }
 
-        private async UniTask AnimationElement()
+        private static async UniTask AnimationElement()
         {
             using (var shapeObjects = new DisposableGameObject(Vector3.zero))
             {
